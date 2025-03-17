4d453a0a9696045d36aa334b46f6f69b6f39248e
Gour <157149036+anonymousthinker@users.noreply.github.com>
2025-03-17 21:44:57 +0530

    refactored the code

--- src/main.js
+++ src/main.js
@@ -6,17 +6,43 @@ class EditorUtilities {
   }
 
   increaseBrightness() {
-    this.imgPixelData.forEach((_, index) => {
-      this.imgPixelData[index] = Math.min(this.imgPixelData[index] * 2, 255);
-    });
-    return [...this.imgMetadata, ...this.imgPixelData];
+    const newImageData = this.imgPixelData.map((pixel) =>
+      Math.min(pixel * 2, 255)
+    );
+
+    return [...this.imgMetadata, ...newImageData];
   }
 
   reduceBrightness() {
-    this.imgPixelData.forEach((_, index) => {
-      this.imgPixelData[index] = Math.max(this.imgPixelData[index] * 0.5, 0);
+    const newImageData = this.imgPixelData.map((pixel) =>
+      Math.max(pixel * 0.5, 0)
+    );
+
+    return [...this.imgMetadata, ...newImageData];
+  }
+
+  increaseContrast() {
+    const threshold = Math.floor(255 / 2);
+
+    const newImageData = this.imgPixelData.map((pixel) => {
+      return pixel < threshold
+        ? Math.max(pixel * 0.8, 0)
+        : Math.min(pixel * 1.5, 255);
     });
-    return [...this.imgMetadata, ...this.imgPixelData];
+
+    return [...this.imgMetadata, ...newImageData];
+  }
+
+  reduceContrast() {
+    const threshold = Math.floor(255 / 2);
+
+    const newImageData = this.imgPixelData.map((pixel) => {
+      return pixel < threshold
+        ? Math.min(pixel * 2, 255)
+        : Math.max(pixel * 0.8, 0);
+    });
+
+    return [...this.imgMetadata, ...newImageData];
   }
 }
 
@@ -33,17 +59,24 @@ class ImageEditor {
       write: true,
       create: true,
     });
+
     const processImage = new TransformStream({
       transform(chunk, controller) {
         const imageChunk = new EditorUtilities(chunk);
+
         const allEdits = {
           increase_brightness: "increaseBrightness",
           reduce_brightness: "reduceBrightness",
+          increase_contrast: "increaseContrast",
+          reduce_contrast: "reduceContrast",
         };
+
         const editedImage = imageChunk[allEdits[editType]]();
+
         controller.enqueue(new Uint8Array(editedImage));
       },
     });
+
     fsFileObject.readable.pipeThrough(processImage).pipeTo(outputFile.writable);
   }
 }
@@ -51,6 +84,7 @@ class ImageEditor {
 const main = async (args) => {
   const [inputFile, outputFile, editType] = args;
   const editor = new ImageEditor(inputFile, outputFile);
+
   await editor.processImage(editType);
 };
 
